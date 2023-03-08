afdda53610e331a5476d43476ef1622f4da231d4
Bigmanreg85 <121136122+Bigmanreg85@users.noreply.github.com>
2023-03-08 22:11:24 +0000

    Add files via upload

---
+++ sa.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import plotly.express as px
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+
+sia = SentimentIntensityAnalyzer()
+
+st.title("Richard Nero's Sentiment Analyser")
+
+st.subheader("Input your text below to analyse the sentiment and chart the output")
+
+user_input = st.text_input("Enter some text:")
+
+scores = sia.polarity_scores(user_input)
+
+l = ['Negative','Neutral','Positive','Overall']
+
+
+new_scores = {}
+
+for i, key in enumerate(scores.keys()):
+	new_key = l[i]
+	new_scores[new_key] = scores[key]
+
+if user_input:
+	# Create a bar chart of the sentiment scores
+	fig = px.bar(x=list(new_scores.keys()), y=list(new_scores.values()))
+
+	# Add chart title and axis labels
+	fig.update_layout(title="Sentiment Scores", xaxis_title="Sentiment", yaxis_title="Score")
+
+	# Display the chart
+	st.plotly_chart(fig, use_container_width=True)
+
+	#st.write(scores)
+
+	if scores['compound'] > 0.05:
+		msg = 'That had a positive sentiment'
+	else:
+		msg = 'That had a negative sentiment'
+
+	st.write(msg)
+	#st.write(scores)b
